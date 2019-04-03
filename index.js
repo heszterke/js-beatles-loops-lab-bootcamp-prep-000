d1480ea310be146d03f0a01d7ab61102c84d912a
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-04-03 18:32:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+function theBeatlesPlay(musicians, instruments){
+  var array[];
+  for (i=0; i < musicians.length; i++){
+  array.push (musicians[1] + " plays " + instrument[1])}
+  return array;
+}
+
