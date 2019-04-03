b8caabdaa739bf9c10cda4b885bb903fbbe002d5
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-04-03 19:17:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(number){
   var array[];}
+  
   do{
     array.push("I love the Beatles!"); number++;
   } while(number < 15);
   
+  return array;
 }
