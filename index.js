dd014e9c43fd7316863bc23038e13163c35c8e2e
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-04-03 19:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function iLoveTheBeatles(number){
   var array[];
 }
   do{
-    array.push("I love the Beatles!"); 
-  } while (number < 15);
+    array.push("I love the Beatles!"); number++;
+  } while (number() < 15);
 }
