f85f46318a066846513d8eeb6405a22c6359c10a
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-04-03 17:37:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array[];
+  for (i=0; i < musicians.length; i++){
+  array.push (musicians[1] + " plays " + instrument[1])}
+  return array;
+}
